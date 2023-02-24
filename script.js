7ad19e0c1c5d2b209399d33db407a61de38a3d59
VaishnaviP666 <122201508+VaishnaviP666@users.noreply.github.com>
2023-02-24 23:58:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+// JavaScript program to connect n
+// ropes with minimum cost
+ 
+function minCost(arr,n)
+{
+    // Create a priority queue
+        let pq = [];
+   
+        // Adding items to the pQueue
+        for (let i = 0; i < n; i++) {
+            pq.push(arr[i]);
+        }   
+           
+        pq.sort(function(a,b){return a-b;});
+         
+        // Initialize result
+        let res = 0;
+   
+        // While size of priority queue
+        // is more than 1
+        while (pq.length > 1) {
+            // Extract shortest two ropes from pq
+            let first = pq.shift();
+            let second = pq.shift();
+   
+            // Connect the ropes: update result
+            // and insert the new rope to pq
+            res += first + second;
+            pq.push(first + second);
+            pq.sort(function(a,b){return a-b;});
+        }
+   
+        return res;
+}
+ 
+// Driver program to test above function
+let len = [4, 3, 2, 6];
+let size = len.length;
+document.write(minCost(len, size));
+ 
+// This code is contributed by avanitrachhadiya2155
+ 
